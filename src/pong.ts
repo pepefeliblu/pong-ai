c55f96af950b09ddb09cee7146629895ec628618
Juan Rueda <juan.rueda@stickerstoke.com>
2023-04-07 08:19:49 -0500

    pong-ai v0.1.1 : Basic state machine and score keeping

--- src/pong.ts
+++ src/pong.ts
@@ -2,24 +2,29 @@ import {Ball} from "./components/ball";
 import {Paddle} from "./components/paddle";
 
 export class Pong {
-    private canvas: HTMLCanvasElement;
-    private context: CanvasRenderingContext2D;
-    private ball: Ball;
+    private readonly canvas: HTMLCanvasElement;
+    private readonly context: CanvasRenderingContext2D;
+    private readonly ball: Ball;
     private player: Paddle;
     private ai: Paddle;
-
     private playerScore: number;
     private aiScore: number;
+    private gameState: GameState;
+    private winningScore: number;
+
 
     constructor(canvas: HTMLCanvasElement) {
+        this.gameState = GameState.StartScreen;
         this.canvas = canvas;
         this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
         this.ball = new Ball(this.canvas.width / 2, this.canvas.height / 2);
         this.player = new Paddle(20, this.canvas.height / 2 - 50, "Player");
         this.ai = new Paddle(this.canvas.width - 40, this.canvas.height / 2 - 50, "AI");
         this.setupInputListeners();
+        this.setupMouseInput();
         this.playerScore = 0;
         this.aiScore = 0;
+        this.winningScore = 1;
     }
 
     start() {
@@ -27,23 +32,34 @@ export class Pong {
     }
 
     private update() {
-        this.ball.move(this.canvas);
-        this.player.move(this.canvas, this.ball);
-        this.ai.move(this.canvas, this.ball);
         this.checkCollision();
+        if (this.gameState === GameState.Playing) {
+            this.ball.move(this.canvas);
+            this.player.move(this.canvas, this.ball);
+            this.ai.move(this.canvas, this.ball);
+        }
         this.draw();
         requestAnimationFrame(() => this.update());
     }
+
 
     private draw() {
         this.context.fillStyle = "black";
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ball.draw(this.context);
-        this.player.draw(this.context);
-        this.ai.draw(this.context);
-        this.drawScoresAndDashedLine();
+        if (this.gameState === GameState.StartScreen) {
+            this.drawStartScreen();
+        } else if (this.gameState === GameState.GameOver) {
+            this.drawGameOverScreen();
+        } else {
+            this.ball.draw(this.context);
+            this.player.draw(this.context);
+            this.ai.draw(this.context);
+            this.drawScoresAndDashedLine();
+        }
     }
+
+
 
     private checkCollision() {
         // Check collision with top and bottom walls
@@ -63,25 +79,27 @@ export class Pong {
         // Check if a player scores
         if (this.ball.x < 0) {
             // AI scores
-            this.resetBall(false, 3);
+            this.aiScore++;
+            if (this.aiScore >= this.winningScore) {
+                this.gameState = GameState.GameOver;
+            } else {
+                this.resetBall(false);
+            }
         } else if (this.ball.x + this.ball.width > this.canvas.width) {
             // Player scores
-            this.resetBall(true, 3);
+            this.playerScore++;
+            if (this.playerScore >= this.winningScore) {
+                this.gameState = GameState.GameOver;
+            } else {
+                this.resetBall(true);
+            }
         }
     }
-    private resetBall(isPlayer: boolean, initialSpeed?: number) {
+    private resetBall(isPlayer: boolean): void {
         this.ball.x = this.canvas.width / 2;
         this.ball.y = this.canvas.height / 2;
         this.ball.dx *= -1;
-        if (initialSpeed) {
-            this.ball.dy = isPlayer ? -initialSpeed : initialSpeed;
-        }
-
-        if (isPlayer) {
-            this.playerScore += 1;
-        } else {
-            this.aiScore += 1;
-        }
+        this.ball.dy = isPlayer ? -Math.random() * 3 - 3 : Math.random() * 3 + 3;
     }
 
 
@@ -105,6 +123,15 @@ export class Pong {
                     break;
             }
         });
+
+        window.addEventListener("keydown", (event) => {
+            if (event.code === "Space") {
+                if (this.gameState === GameState.StartScreen || this.gameState === GameState.GameOver) {
+                    this.resetGame();
+                    this.gameState = GameState.Playing;
+                }
+            }
+        });
     }
 
     private drawScoresAndDashedLine() {
@@ -119,4 +146,84 @@ export class Pong {
         }
     }
 
+    private setupMouseInput(): void {
+        let dragging = false;
+
+        const isMouseOnPaddle = (event: MouseEvent): boolean => {
+            const rect = this.canvas.getBoundingClientRect();
+            const mouseX = event.clientX - rect.left;
+            const mouseY = event.clientY - rect.top;
+
+            return (
+                mouseX >= this.player.x &&
+                mouseX <= this.player.x + this.player.width &&
+                mouseY >= this.player.y &&
+                mouseY <= this.player.y + this.player.height
+            );
+        };
+
+        this.canvas.addEventListener("mousedown", (event) => {
+            if (isMouseOnPaddle(event)) {
+                dragging = true;
+            }
+        });
+
+        this.canvas.addEventListener("mouseup", () => {
+            dragging = false;
+        });
+
+        this.canvas.addEventListener("mousemove", (event) => {
+            if (dragging) {
+                const rect = this.canvas.getBoundingClientRect();
+                const mouseY = event.clientY - rect.top;
+                this.player.y = mouseY - this.player.height / 2;
+
+                // Prevent the player paddle from moving off the canvas
+                this.player.y = Math.max(0, Math.min(this.player.y, this.canvas.height - this.player.height));
+            }
+        });
+    }
+
+    private drawStartScreen() {
+        this.context.fillStyle = "white";
+        this.context.font = "32px Arial";
+        this.context.textAlign = "center";
+        this.context.fillText("Press SPACE to start", this.canvas.width / 2, this.canvas.height / 2);
+    }
+
+    private drawGameOverScreen() {
+        this.context.fillStyle = "white";
+        this.context.font = "32px Arial";
+        this.context.textAlign = "center";
+        const winner = this.playerScore >= this.winningScore ? "You" : "AI";
+        this.context.fillText(`${winner} won!`, this.canvas.width / 2, this.canvas.height / 2 - 40);
+        this.context.fillText("Press SPACE to restart", this.canvas.width / 2, this.canvas.height / 2 + 40);
+    }
+
+
+    private resetScores() {
+        this.playerScore = 0;
+        this.aiScore = 0;
+    }
+
+    private resetGame() {
+        this.resetBall(this.playerScore >= this.winningScore);
+        this.resetPaddles();
+        this.resetScores();
+    }
+
+    private resetPaddles() {
+        this.player.x = 20;
+        this.player.y = this.canvas.height / 2 - 50;
+        this.ai.x = this.canvas.width - 40;
+        this.ai.y = this.canvas.height / 2 - 50;
+    }
+
 }
+
+enum GameState {
+    StartScreen,
+    Playing,
+    GameOver,
+}
+
